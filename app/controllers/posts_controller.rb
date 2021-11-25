d53bdc8a26f6341f9b08d642ad8efcca7d2cdc87
Shota Nagayama <30305829kasukabe@gmail.com>
2021-11-25 09:23:03 +0900

    一覧表示でエラー

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,16 @@
 class PostsController < ApplicationController
   def index
-
+    @posts = Post.order(id: :asc)
   end
 
   def create
-    post = Post.create!(post_params)
-    redirect_to post
+    @post = Post.create!(post_params)
+    redirect_to root_path
   end
 
   private
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:body)
   end
 
 end
